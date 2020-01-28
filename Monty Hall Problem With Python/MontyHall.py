221c6aac59088c49e1738cd454777f3ba67c2b35
MSH <mjsh635@gmail.com>
2020-01-28 16:25:19 -0500

    Continue development of python monty hall problem

--- Monty Hall Problem With Python/MontyHall.py
+++ Monty Hall Problem With Python/MontyHall.py
@@ -7,56 +7,82 @@ Menu to ask to Play or Simulate
 """
 #import libraries that will be needed
 import random
+import time
 
 
 
-def StartRandomDoorPositions() -> 'Bool array':
+def StartRandomDoorPositions() -> 'array of doors':
     """
     When called will return bool[] with all set false except 1 randomly chosen door which will be set true
     """
 
-    montysRandomDoor = random.randint(0,2)
-    montysDoors = [False,False,False]
-    montysDoors[montysRandomDoor] = True
-    return montysDoors
+    randint = random.randint(0,2)
+    randomDoors = [False,False,False]
+    randomDoors[randint] = True
+    return randomDoors
 
-def PlayersGuess() -> 'int of door guessed':
+def PlayersGuess(doors) -> 'int of door guessed':
     """
     When called will ask for the player to
     """
+    while True: 
+
+            #take the user input
+            print("Please pick a door....1 | 2 | 3")
+            potentialplayerGuess = input('')
+
+            #filter it to see if it is a digit, and falls in the doors bounds
+
+            if potentialplayerGuess.isdigit():
+                if int(potentialplayerGuess) >= 1 and int(potentialplayerGuess) <= 3:
+
+                    #if its a valid guess store the guess and set valid Entry = True
+
+                    playerGuess = int(potentialplayerGuess)
+                    break
+                else:
+                    print("""That is not a door, please enter 1, 2, or 3... 
+                    """)
+                    
+            else:
+                print("""That was not a number, please enter a number...
+                """)
+            time.sleep(2)
+    return playerGuess        
+    
+
+def MontyReveals(doors,playerGuess) -> "Monty's opened door":
+    """
+    take doors in, and the door the player guess, reveals a false door that isnt the players guess
+    """
+    for MontyRevealed in range(len(doors)):
+        if playerGuess == MontyRevealed:
+            continue
+        elif doors[MontyRevealed] == False:
+            return MontyRevealed
+
+
+def SwitchRequest(doors, revealedDoor, oldGuess)-> "players new guess":
+    """
+    show the doors, and montys opened door, ask if they want to switch, return new guess if they want to switch
+    """
+    playerNewGuess = 1
+    return playerNewGuess
+
+def Results(newGuess, doors) -> "string of results":
+    result = True
+    return f"You got the: {result}"
+
+
+    
 
 def Play():
     """
     starts a playable version of the game, to see if you can out smart monty.
     """
-    randomizedDoors = StartRandomDoorPositions()
+    doors = StartRandomDoorPositions()
+    guess = PlayersGuess(doors)
+    revealed = MontyReveals(doors, guess)
+    newGuess = SwitchRequest(doors, revealed, guess)
+    print(Results(newGuess,doors))
 
-    validEntry = False
-
-    #Ensure that the player enters a valid door selection
-
-    while not validEntry: 
-
-        #take the user input
-        print("Please pick a door....1 | 2 | 3")
-        potentialplayerGuess = input('')
-
-        #filter it to see if it is a digit, and falls in the doors bounds
-
-        if potentialplayerGuess.isdigit():
-            if int(potentialplayerGuess) >= 1 and int(potentialplayerGuess) <= 3:
-
-                #if its a valid guess store the guess and set valid Entry = True
-
-                playerGuess = int(potentialplayerGuess)
-                validEntry = True
-            else:
-                validEntry = False
-                print("please enter 1, 2, or 3")
-        else:
-            validEntry = False
-            print("please enter a number between 1 and 3")
-
-    print(f"player guessed:  {playerGuess}")
-
-Play()
