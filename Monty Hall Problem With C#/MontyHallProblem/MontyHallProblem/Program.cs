d79354d9d8605b4c06714aebe813cc74b68504e0
MSH <mjsh635@gmail.com>
2020-01-31 08:31:02 -0500

    Main method to allow playable console version at the moment

--- Monty Hall Problem With C#/MontyHallProblem/MontyHallProblem/Program.cs
+++ Monty Hall Problem With C#/MontyHallProblem/MontyHallProblem/Program.cs
@@ -14,9 +14,13 @@ namespace MontyHallProblem
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            while (true)
+            {
+                Control game = new Control();
+
+                Console.WriteLine(game.Play());
+                
+            }
         }
     }
 }
